af87064f286688c1334cbdd5cb8c72aeb1685b09
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-09-09 14:51:58 +0400

    сделал регистрацию User

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+// bcrypt - это библиотека позволяет шифровать пароль с фронтенда
+import bcrypt from "bcrypt";
+import { registerValidation } from "./validation/auth.js";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
 
 // эта библиотека позволяет работать с MONGODB
 mongoose
   .connect(
-    "mongodb+srv://lehaleonov52:6319cccfc3294c25191db7ec@cluster0.qpz6qye.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://ST:wwwsss@cluster0.yx8xczp.mongodb.net/blog?retryWrites=true&w=majority"
   )
-  .then(() => console.log("DB"))
-  .catch((err) => console.log("DB ERROR" + err));
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB ERROR", err));
 
 const app = express();
 
 // позволит читать JSON в запросах
 app.use(express.json());
 
-// req - будет хранится инфа о том что мне прислал клиент,в res я обьясняю что я буду передовать клиенту
-// expess приложение которое хранится в app
-// тут обьясняю что когда придет что-то (роут)-что вернуть пользователю
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
-
 // авторизация
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-// БЛАГОДАРЯ JWT ШИФРУЮ ИНФУ
-  const token = jwt.sign(
-    {
+app.post("/auth/register", registerValidation, async (req, res) => {
+ try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+  
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    // отличный способ шифрования пароля
+    const passwordHash = await bcrypt.hash(password, salt);
+  
+    // документ на создания нового пользователя
+    const doc = new UserModel({
       email: req.body.email,
-      fullName: "Алекс Фергюсон",
-    },
-    "secret123"
-  );
-  res.json({
-    success: true,
-    token,
-  });
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+  
+    // создаю самого пользователя в mongodb
+    const user = await doc.save();
+  
+    res.json(user);
+ } catch (error) {
+    // это передаю для себя 
+    console.log(error);
+    //ЭТО ДЛЯ ПОЛЬЗОВАТЕЛЯ-пришёл ответ ввиде ошибке и вот ифа о ошибке
+    res.status(500).json({
+        message:"Не удалось зарегистрироваться"
+    });
+ }
 });
 
 // обьясняю на какой порт прикрепить app - можно указать лбой ВТОРОЙ ПАРАМЕТР функция - если произошла ошибка
