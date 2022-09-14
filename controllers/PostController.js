3db6266a70e3bfba6f5d2b9cb1b95913789dd0a3
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-09-14 11:21:50 +0400

    logic posts and users

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,11 +1,92 @@
 import PostModel from "../models/Post.js";
 
-// функционал для создания статьи
+// получение всех постов
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Неудалось получить статьи",
+    });
+  }
+};
+// поучение одного поста
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      },
+      (error, doc) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Неудалось вернуть статью",
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+        res.json(doc);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Неудалось получить статьи",
+    });
+  }
+};
+// удаление одной статьи
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (error, doc) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Не удалось удалить статью",
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Неудалось получить статьи",
+    });
+  }
+};
+// функционал для создания одной статьи
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
       title: req.body.title,
-      text: req.body.text,
+      text: req.body.title,
       tags: req.body.tags,
       imageUrl: req.body.imageUrl,
       user: req.userId,
@@ -16,6 +97,31 @@ export const create = async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: "Неудалось создать статью",
+    });
+  }
+};
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.title,
+        tags: req.body.tags,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+      }
+    );
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+    message: "Не удалось обновить статью",
     });
   }
 };
